7bd8ea2c3cca35c83f6574eeb8b03334b5ad0492
pranavp1551 <49173066+pranavp1551@users.noreply.github.com>
2023-06-26 13:11:40 -0400

    Update LinkedStackTest.java

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,11 +1,4 @@
-/**
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor 
-// will I accept the actions of those who do.
-// -- Pranav Poodari (pranavp1551)
- */
+
 package towerofhanoi;
 
 import java.util.EmptyStackException;
